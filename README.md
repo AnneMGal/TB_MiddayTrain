930d019a5d6c644429c1e121ffcb03b5de9befda
AnneMGal <amg386@nyu.edu>
2017-02-21 22:13:19 -0500

    initial commit of midday train twitter bot

---
+++ README.md
@@ -0,0 +1,10 @@
+# this is my midday_train twitter bot
+
+every minute, it tweets the name of a city in the us
+
+you can also interact with it by:
+ - liking
+ - following
+ - direct messaging
+ 
+ enjoy!
---
+++ middaytrain.js
@@ -0,0 +1,200 @@
+var TwitterAPI = require('node-twitter-api');
+var util = require('util');
+var fs = require('fs');
+var sleep = require('sleep');
+//var rita = require('rita');
+
+var counterBuffer = fs.readFileSync("counter.txt");
+//turning into integer
+var i = parseInt(counterBuffer.toString());
+//var counter = parseInt(fs.readFileSync("counter.txt").toString());
+
+
+// second account
+var secrets = JSON.parse(fs.readFileSync("../../__secrets/midday_train.secrets.json"));
+
+var cKey = secrets["cKey"];
+var cSecret = secrets["cSecret"];
+var accessToken = secrets["accessToken"];
+var tokenSecret = secrets["tokenSecret"];
+
+
+var myScreenName = "midday_train"
+
+// create a twitter API object
+var twitter = new TwitterAPI({
+    consumerKey: cKey,
+    consumerSecret: cSecret});
+    
+// register onData() as handler for getStream() events
+twitter.getStream("user", {}, accessToken, tokenSecret, onData);    
+
+    
+var citiesData = JSON.parse(fs.readFileSync("us_cities.json"));
+var cities = citiesData["cities"];
+var states = citiesData["state"];
+
+var timer_secs = 60;
+setInterval(tweet_names, timer_secs*1000);
+
+function onData(error, streamEvent) {
+
+	console.log('some event');
+
+    // a few different cases.
+    // case 1: if the object is empty, simply return
+    if (Object.keys(streamEvent).length === 0) {
+
+        return;
+    }
+
+    // "event" key present for favorites and new followers
+    else if (streamEvent.hasOwnProperty('event')) {
+        var sourceHandle = streamEvent['source']['screen_name'];
+        // a new follower!
+        if (streamEvent['event'] == 'follow') {
+            console.log("followed by @" + sourceHandle);
+            twitter.statuses(
+                "update",
+               {"status": "you are welcome to tag along, @" + sourceHandle},
+               accessToken,
+               tokenSecret,
+               function (err, data, resp) { console.log(err); }
+            );
+        }
+        else if (streamEvent['event'] == 'favorite') {
+            var favoritedTweetId = streamEvent['target']['id_str'];
+            var screenName = streamEvent['source']['screen_name'];
+            console.log("tweet id ",
+                    favoritedTweetId,
+                    " favorited by @",
+                    screenName);
+            
+            
+            var MAX_DICE = 3;
+            
+            // a random int between [0 and MAX_DICE]
+            var diceRoll = Math.floor(Math.random()*MAX_DICE);
+            
+            console.log("rolled: " + diceRoll.toString());
+            
+            if (diceRoll == 0) {
+            	twitter.statuses(
+             	   "update",
+             	  {"status": cities[i - 1].city + ", " + cities[i - 1].state + " is a great place to visit 1, @" + screenName},
+            	   accessToken,
+             	  tokenSecret,
+             	  function (err, data, resp) { console.log(err); }
+            	); 
+            } else if (diceRoll == 1) {
+            	twitter.statuses(
+             	   "update",
+             	  {"status": cities[i - 1].city + ", " + cities[i - 1].state + " is a great place to visit 2, @" + screenName},
+            	   accessToken,
+             	  tokenSecret,
+             	  function (err, data, resp) { console.log(err); }
+            	); 
+            } else if (diceRoll == 2) {
+            	twitter.statuses(
+             	   "update",
+             	  {"status": cities[i - 1].city + ", " + cities[i - 1].state + " is a great place to visit 3, @" + screenName},
+            	   accessToken,
+             	  tokenSecret,
+             	  function (err, data, resp) { console.log(err); }
+            	); 
+            } else {
+            	console.log("dice error");
+            }
+            
+            
+            
+            
+            
+        }
+    }
+
+    // 'direct_message' key indicates this is an incoming direct message
+    else if (streamEvent.hasOwnProperty('direct_message')) {
+        var dmText = streamEvent['direct_message']['text'];
+        var senderName = streamEvent['direct_message']['sender']['screen_name'];
+        // streaming API sends us our own direct messages! skip if we're
+        // the sender.
+        if (senderName == myScreenName) {
+            return;
+        }
+        // send a response!
+        twitter.direct_messages(
+            'new',
+            {
+                "screen_name": senderName,
+                "text": "enjoy your trip '" + dmText + "'!"
+            },
+            accessToken,
+            tokenSecret,
+            function (err, data, resp) { console.log(err); }
+        );
+    }
+
+    // otherwise, this was probably an incoming tweet. we'll check to see if
+    // it starts with the handle of the bot and then send a response.
+    else if (streamEvent.hasOwnProperty('text')) {
+        if (streamEvent['text'].startsWith("@"+myScreenName+" ")) {
+            var tweetId = streamEvent['id_str'];
+            var tweeterHandle = streamEvent['user']['screen_name'];
+            //random dice roll
+            var diceRoll = Math.floor(Math.random()*100)+1;
+            twitter.statuses(
+                "update",
+                //{"status": "@" + tweeterHandle + "+diceRoll.toString+take lots of pictures!",
+               {"status": "@" + tweeterHandle + ", " + diceRoll.toString() + ", hey, hey",
+                "in_reply_to_status_id": tweetId},
+               accessToken,
+               tokenSecret,
+               function (err, data, resp) { console.log(err); }
+            );
+        }
+    }
+
+    // if none of the previous checks have succeeded, just log the event
+    else {
+        console.log(streamEvent);
+    }
+} 
+
+
+// tweet city destination
+function tweet_names() {
+
+	//var counterBuffer = fs.readFileSync("counter.txt");
+	//turning into integer
+	//var i = parseInt(counterBuffer.toString());
+	
+	//var counter = parseInt(fs.readFileSync("counter.txt").toString());
+
+twitter.statuses("update",
+    {"status": "Hello. Where are you headed? I am going to " + cities[i].city + ", " + cities[i].state + "."},
+    accessToken,
+    tokenSecret,
+    function(error, data, response) {
+        if (error) {
+            console.log("something went wrong: " + util.inspect(error));
+        }
+    }
+);
+
+
+
+		//console.log("sleeping");
+		//sleep.sleep(10);
+		i++;
+		var counterString = i.toString();
+		fs.writeFileSync("counter.txt", counterString, "utf8");
+
+	//}
+	
+
+}
+
+
+
+
